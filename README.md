cb907f6a6007585f6c66ed9d3187e8bdaf66c9f0
Stevie Howard <zahoward@gmail.com>
2019-01-28 23:33:45 -0800

    Web server setup (#1)
    * Update README with usage details
    
    * Setup basic webserver and flags

--- README.md
+++ README.md
@@ -28,6 +28,26 @@ If you've already installed this repository as per [installation](#installation)
 
   `go get -u $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
 
+## Usage
+
+In its production environment, the HTTP server will communicate with a Transaction Server and a Database Server.
+The ports on which to communicate **must** be specified at runtime, using the following flags:
+
+* `-dbport`
+
+eg. `-dbport=4564`
+
+* `-txport`
+
+eg. `-txport=5675`
+
+> The HTTP server runs on port 80 by default.
+
+The HTTP server can be started up using the `go run` command.
+
+eg. `go run $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server -dbport=4564 -txport=5675`
+
+
 ## See Also
 
 Components and documentation of the day trading system:
@@ -38,5 +58,5 @@ Components and documentation of the day trading system:
 * [Common Library](https://github.com/kurtd5105/SENG-468-Common-Lib)
 * [Transaction Server](https://github.com/kurtd5105/SENG-468-Transaction-Server)
 * [Quote Management Server](https://github.com/sterlinglaird/)
-* [Database Server](https://github.com/sterlinglaird/)
+* [Database Server](https://github.com/sterlinglaird/SENG-468-Database-Server)
 * [Workload Generator](https://github.com/dukeng/SENG-468-Workload-Generator)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// ServerState holds information about the server's state and configuration
+type ServerState struct {
+	dbPort   int
+	txPort   int
+	httpPort int
+}
+
+var serverState = ServerState{}
+
+func init() {
+	// Parse and process CLI flags
+	flag.IntVar(&serverState.dbPort, "dbport", -1, "[REQUIRED] the port on which the DATABASE server is running, eg. --dbport=8080")
+	flag.IntVar(&serverState.httpPort, "httpport", 80, "[optional -- default is port 80] the port on which *this* HTTP server is running, eg. --httpport=80")
+	flag.IntVar(&serverState.txPort, "txport", -1, "[REQUIRED] the port on which the TRANSACTION server is running, eg. --txport=8082")
+	flag.Parse()
+
+	// Force flags as required
+	if serverState.dbPort == -1 || serverState.txPort == -1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// Display server state values
+	fmt.Printf("%+v\n", serverState)
+
+	// Fire up the server
+	portNumString := ":" + strconv.Itoa(serverState.httpPort)
+	http.HandleFunc("/", requestHandler)
+	fmt.Printf("HTTP server listening on http://localhost:%d\n", serverState.httpPort)
+	http.ListenAndServe(portNumString, nil)
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HTTP server running right meow.\n\n")
+	fmt.Fprintf(w, "The request received:\n\n")
+	fmt.Fprintf(w, "%s\n", r.Method)
+	fmt.Fprintf(w, "%s\n", r.Body)
+}

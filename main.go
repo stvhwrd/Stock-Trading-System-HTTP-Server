29ea76fef675551733df9dce1d60b1100718385e
stvhwrd <stvhwrd@outlook.com>
2019-01-29 08:08:09 -0800

    Add validation templates

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+
+	commonlib "github.com/kurtd5105/SENG-468-Common-Lib"
 )
 
 // ServerState holds information about the server's state and configuration
@@ -25,7 +27,7 @@ func init() {
 	flag.IntVar(&serverState.txPort, "txport", -1, "[REQUIRED] the port on which the TRANSACTION server is running, eg. --txport=8082")
 	flag.Parse()
 
-	// Force flags as required
+	// Enforce required flags
 	if serverState.dbPort == -1 || serverState.txPort == -1 {
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -33,22 +35,45 @@ func init() {
 }
 
 func main() {
-	// Display server state values
-	fmt.Printf("%+v\n", serverState)
+	// Display server state values / config
+	log.Printf("LOG: %+v\n", serverState)
 
-	// Fire up the server
-	portNumString := ":" + strconv.Itoa(serverState.httpPort)
-	http.HandleFunc("/", requestHandler)
-	http.HandleFunc("/ui", uiHandler)
-	fmt.Printf("HTTP server listening on http://localhost:%d\n", serverState.httpPort)
-	http.ListenAndServe(portNumString, nil)
+	// Using multiplexer to map endpoints to handler functions
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ui", uiHandler)
+	mux.HandleFunc("/", commandHandler)
+
+	log.Printf("LOG: HTTP server listening on http://localhost:%d\n", serverState.httpPort)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(serverState.httpPort), mux))
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HTTP server running right meow.\n\n")
-	fmt.Fprintf(w, "The request received:\n\n")
-	fmt.Fprintf(w, "%s\n", r.Method)
-	fmt.Fprintf(w, "%s\n", r.Body)
+// commandHandler processes a JSON command and forwards it to the transaction server
+func commandHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("LOG: Received %s request to \"/ui\" endpoint\n", r.Method)
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusBadRequest)
+	case "POST":
+
+		// Read request body
+		requestBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			panic(err)
+		}
+		defer r.Body.Close()
+
+		// Parse and validate request
+		commandID, parameters := commonlib.GetCommandFromMessage(requestBody)
+		if validateCommandAndParameters(commandID, parameters) {
+			forwardMessageToTransactionServer(commandID, parameters)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
 }
 
 // uiHandler presents an HTML webpage for a user to manually enter commands, manage their account, etc.
@@ -57,8 +82,86 @@ func uiHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "POST":
-		requestHandler(w, r)
+		commandHandler(w, r)
 	case "GET":
 		http.ServeFile(w, r, "www/index.html")
 	}
+}
+
+// getDumplogForUser retrieves the transaction history for a specific user from the database and saves it to a logfile
+func getDumplogForUser(userid string) {
+	// TODO: Open connection to DB
+	// TODO: Query DB eg. db.Query("SELECT * FROM transactions WHERE userid = $1", userid)
+	// TODO: Write returned rows to <filename>
+	// TODO: Close connection to DB
+}
+
+// getDumplogForAll retrieves the transaction history for all users from the database and saves it to a logfile
+func getDumplogForAll() {
+	// TODO: Open connection to DB
+	// TODO: Query DB eg. db.Query("SELECT * FROM transactions)
+	// TODO: Write returned rows to <filename>
+	// TODO: Close connection to DB
+}
+
+// forwardMessageToTransactionServer forwards the received message to the Transaction Server
+func forwardMessageToTransactionServer(commandID uint8, parameters commonlib.CommandParameter) (bool, error) {
+	sendableCommand := commonlib.GetSendableCommand(commandID, parameters)
+	response, err := commonlib.SendCommand("GET", "application/json", serverState.txPort, sendableCommand)
+
+	log.Println("LOG: Sent command: ", sendableCommand)
+	log.Println("LOG: Received response: ", response)
+
+	if err != nil {
+		log.Println("LOG: Forwarding message to Transaction Server failed with error: ", err)
+		return false, err
+	}
+	log.Println("LOG: Forwarding message to Transaction Server succeeded.")
+	return true, err
+}
+
+// saveToFile exports a byte array to a file on local disk
+func saveToFile(content []byte, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	bytesWritten, err := file.Write(content)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("LOG: Wrote %d bytes to logfile: %q\n", bytesWritten, filename)
+}
+
+// validateCommand ensures that the command is one of the known/valid commands and has its necessary parameters
+func validateCommandAndParameters(commandID uint8, parameters commonlib.CommandParameter) bool {
+	// TODO: Ensure all parameters pertaining to the specific command are present and valid
+
+	return true
+}
+
+// validateAmount ensures that the amount specified in command is valid
+func validateAmount(amount string) bool {
+	// TODO: Must be non-negative
+	// TODO: Must not contain non-numerical characters (including "$")
+	// TODO: Must contain two decimal places <---(Do we want to round, or reject, if <> 2 decimal places?)
+
+	return true
+}
+
+// validateUserID ensures that the user specified in command is valid
+func validateUserID(userID string) bool {
+	// TODO: Create user if not exists <--(or does the client issue a "CREATE" command?)
+
+	return true
+}
+
+// validateStockSymbol ensures that the stock symbol specified in command is valid
+func validateStockSymbol(stockSymbol string) bool {
+	// TODO: Must be 1 - 3 alphanumeric, case insensitive
+
+	return true
 }

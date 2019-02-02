7bfd7eb0cbef579dc52d95b93167350d7ef3eb2b
Stevie Howard <zahoward@gmail.com>
2019-02-02 11:31:00 -0800

    Remove redundant multiplexer and endpoint

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ type ServerState struct {
 var serverState = ServerState{}
 
 func init() {
-	// Parse and process CLI flags
+	// Parse and process CLI flags (type checking is implicit)
 	flag.IntVar(&serverState.accountDbPort, "accountdbport", -1, "[REQUIRED] the port on which the USER ACCOUNT DATABASE server is running, eg. --accountdbport=8080")
 	flag.IntVar(&serverState.accountDbPort, "loggingdbport", -1, "[REQUIRED] the port on which the LOGGING DATABASE server is running, eg. --loggingdbport=8081")
 	flag.IntVar(&serverState.httpPort, "httpport", 80, "[optional -- default is port 80] the port on which *this* HTTP server is running, eg. --httpport=80")
@@ -37,78 +37,57 @@ func init() {
 }
 
 func main() {
-	// Display server state values / config
-	log.Printf("LOG: %+v\n", serverState)
+	log.Printf("LOG: Server starting with config: %+v\n", serverState)
 
-	// Using multiplexer to map endpoints to handler functions
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ui", uiHandler)
-	mux.HandleFunc("/", commandHandler)
-
-	log.Printf("LOG: HTTP server listening on http://localhost:%d\n", serverState.httpPort)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(serverState.httpPort), mux))
+	// Fire up server
+	log.Printf("LOG: HTTP server listening on http://localhost:%d/\n", serverState.httpPort)
+	http.HandleFunc("/", requestRouter)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(serverState.httpPort), nil))
 }
 
-// commandHandler processes a JSON command and forwards it to the transaction server
-func commandHandler(w http.ResponseWriter, r *http.Request) {
-
-	log.Printf("LOG: Received %s request to \"/ui\" endpoint\n", r.Method)
-
+// requestRouter routes the request to the appropriate handler based on its HTTP method
+func requestRouter(w http.ResponseWriter, r *http.Request) {
+	log.Printf("LOG: Received %s request.\n", r.Method)
 	switch r.Method {
+	case "POST":
+		commandProcessor(w, r)
 	case "GET":
+		userInterfaceHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+// commandProcessor processes a JSON command and forwards it to the transaction server
+func commandProcessor(w http.ResponseWriter, r *http.Request) {
+	// Read request body
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-	case "POST":
+		panic(err)
+	}
+	defer r.Body.Close()
 
-		// Read request body
-		requestBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			panic(err)
-		}
-		defer r.Body.Close()
+	// Parse and validate request
+	commandID, parameters := commonlib.GetCommandFromMessage(requestBody)
 
-		// Parse and validate request
-		commandID, parameters := commonlib.GetCommandFromMessage(requestBody)
-		if validateCommandAndParameters(commandID, parameters) {
-			forwardMessageToTransactionServer(commandID, parameters)
-		} else {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-		}
+	if validateCommandAndParameters(commandID, parameters) {
+		// TODO: Check return value and handle retries
+		forwardCommandToTransactionServer(commandID, parameters)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		// TODO: Handle failed validations manually
+		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
-// uiHandler presents an HTML webpage for a user to manually enter commands, manage their account, etc.
-func uiHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("LOG: Received %s request to \"/ui\" endpoint\n", r.Method)
-
-	switch r.Method {
-	case "POST":
-		commandHandler(w, r)
-	case "GET":
-		http.ServeFile(w, r, "www/index.html")
-	}
+// userInterfaceHandler serves the user interface HTML file
+func userInterfaceHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "www/index.html")
 }
 
-// getDumplogForUser retrieves the transaction history for a specific user from the database and saves it to a logfile
-func getDumplogForUser(userid string) {
-
-	// TODO: Open connection to DB
-	// TODO: Query DB eg. db.Query("SELECT * FROM transactions WHERE userid = $1", userid)
-	// TODO: Write returned rows to <filename>
-	// TODO: Close connection to DB
-}
-
-// getDumplogForAll retrieves the transaction history for all users from the database and saves it to a logfile
-func getDumplogForAll() {
-	// TODO: Open connection to DB
-	// TODO: Query DB eg. db.Query("SELECT * FROM transactions)
-	// TODO: Write returned rows to <filename>
-	// TODO: Close connection to DB
-}
-
-// forwardMessageToTransactionServer forwards the received message to the Transaction Server
-func forwardMessageToTransactionServer(commandID uint8, parameters commonlib.CommandParameter) (bool, error) {
+// forwardCommandToTransactionServer forwards the received message to the Transaction Server
+func forwardCommandToTransactionServer(commandID uint8, parameters commonlib.CommandParameter) (bool, error) {
 	sendableCommand := commonlib.GetSendableCommand(commandID, parameters)
 	response, err := commonlib.SendCommand("GET", "application/json", serverState.txPort, sendableCommand)
 
@@ -129,7 +108,6 @@ func saveToFile(content []byte, filename string) {
 	if err != nil {
 		panic(err)
 	}
-
 	defer file.Close()
 
 	bytesWritten, err := file.Write(content)
@@ -167,4 +145,21 @@ func validateStockSymbol(stockSymbol string) bool {
 	// TODO: Must be 1 - 3 alphanumeric, case insensitive
 
 	return true
+}
+
+// getDumplogForUser retrieves the transaction history for a specific user from the database and saves it to a logfile
+func getDumplogForUser(userid string) {
+
+	// TODO: Open connection to DB
+	// TODO: Query DB eg. db.Query("SELECT * FROM transactions WHERE userid = $1", userid)
+	// TODO: Write returned rows to <filename>
+	// TODO: Close connection to DB
+}
+
+// getDumplogForAll retrieves the transaction history for all users from the database and saves it to a logfile
+func getDumplogForAll() {
+	// TODO: Open connection to DB
+	// TODO: Query DB eg. db.Query("SELECT * FROM transactions)
+	// TODO: Write returned rows to <filename>
+	// TODO: Close connection to DB
 }

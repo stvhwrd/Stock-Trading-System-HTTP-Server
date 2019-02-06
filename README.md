a340c7fd4f8b020bb679bbb6bcc4149faf9862b1
Stevie Howard <zahoward@gmail.com>
2019-02-06 07:35:32 -0800

    Rewrite server (#8)
    * Server setup
    
    * Use .env for server configurations
    
    * Update README
    
    * Use integers for port number env vars
    
    * Forward messages to transaction server (no validation)
    
    * Ignore eponymous binaries
    
    * Micro-refactor - change names etc
    
    * Update parameter type to keep up with Common-Lib
    
    * Nix redundant panic
    
    * Update usage instructions in README
    
    * Use strings for port numbers as per commonlib
    
    * Update ports to match docker-compose

--- README.md
+++ README.md
@@ -10,67 +10,51 @@ The HTTP server component of the day trading system designed and built for SENG 
 
 1. Clone this repository.
 
-  * With HTTPS:
+-   With HTTPS:
 
-  `git clone https://github.com/stvhwrd/SENG-468-HTTP-Server.git $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
+`git clone https://github.com/stvhwrd/SENG-468-HTTP-Server.git $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
 
-  * or with SSH:
+-   or with SSH:
 
-  `git clone git@github.com:stvhwrd/SENG-468-HTTP-Server.git $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
+`git clone git@github.com:stvhwrd/SENG-468-HTTP-Server.git $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
 
-2. Build the Go files.
-  
-  `go build $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
+2. Install the dependencies.
+
+`go get -v -u github.com/joho/godotenv`
+
+3. Build the Go files.
+
+`go build $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
+
+## Configuration
+
+In its production environment, the HTTP server will communicate with a Transaction Server, a User Account Database Server, and a Logging Server.
+
+The ports on which to communicate **must** be specified at runtime, using a `.env` file.
+
+The file should be located in the same directory as `main.go` (as it is by default in this repository).
+
+## Usage
+
+The HTTP server can be started up using the `go run` command after building.
+
+eg. `cd $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server && go build && go run .`
 
 ## Updating
 
 If you've already installed this repository as per [installation](#installation), you may update in-place.
 
-  `go get -u $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
-
-## Usage
-
-The HTTP server can be started up using the `go run` command.
-
-In its production environment, the HTTP server will communicate with a Transaction Server, a User Account Database Server, and a Logging Server.
-
-The ports on which to communicate **must** be specified at runtime, using the following flags:
-
-* `accountDbPort`  -- [**REQUIRED**] the port on which the USER ACCOUNT DATABASE server is running
-    
-    - eg. `--accountdbport=8080`
-
-* `loggingDbPort` -- [**REQUIRED**] the port on which the LOGGING DATABASE server is running
-    
-    - eg. `--loggingdbport=8081`
-
-* `txPort` -- [**REQUIRED**] the port on which the TRANSACTION server is running
-    
-    - eg. `--txport=8082`
-
-The HTTP server will listen on localhost's port 80 by default, but this can also be set at runtime with an optional flag:
-
-* `httpPort` -- [optional] the port on which *this* HTTP server is running
-    
-    - eg. `--httpport=80`
-
-
-### Example
-
-From your local directory containing `main.go`:
-
-`go run main.go -accountdbport=8080 -loggingdbport=8081 -txport=8082`
-
+`go get -u $GOPATH/src/github.com/stvhwrd/SENG-468-HTTP-Server`
 
 ## See Also
 
 Components and documentation of the day trading system:
 
-* [Common Library](https://github.com/kurtd5105/SENG-468-Common-Lib)
-* [Database Server](https://github.com/sterlinglaird/SENG-468-Database-Server)
-* [HTTP Server](https://github.com/stvhwrd/SENG-468-HTTP-Server)
-* [Logging Server](https://github.com/dukeng/SENG-468-Logging-Server)
-* [Project Documentation](https://github.com/stvhwrd/SENG-468-Documentation)
-* [Transaction Server](https://github.com/kurtd5105/SENG-468-Transaction-Server)
-* [Web Client](https://github.com/dukeng/SENG-468-Web-Client)
-* [Workload Generator](https://github.com/dukeng/SENG-468-Workload-Generator)
+- [Common Library](https://github.com/kurtd5105/SENG-468-Common-Lib)
+- [Database Server](https://github.com/sterlinglaird/SENG-468-Database-Server)
+- [HTTP Server](https://github.com/stvhwrd/SENG-468-HTTP-Server)
+- [Logging Server](https://github.com/dukeng/SENG-468-Logging-Server)
+- [Project Documentation](https://github.com/stvhwrd/SENG-468-Documentation)
+- [Transaction Server](https://github.com/kurtd5105/SENG-468-Transaction-Server)
+- [Web Client](https://github.com/dukeng/SENG-468-Web-Client)
+- [Workload Generator](https://github.com/dukeng/SENG-468-Workload-Generator)
--- main.go
+++ main.go
@@ -1,165 +1,116 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
-	commonlib "github.com/kurtd5105/SENG-468-Common-Lib"
+	"github.com/joho/godotenv"
 )
 
-// ServerState holds information about the server's state and configuration
-type ServerState struct {
-	accountDbPort int
-	loggingDbPort int
-	txPort        int
-	httpPort      int
+// ServerConfiguration holds information about a server's network configuration
+type ServerConfiguration struct {
+	ipAddress string
+	port      string
 }
 
-var serverState = ServerState{}
+// ServerConfigurations holds information about the system's servers' network configurations
+type ServerConfigurations struct {
+	database    ServerConfiguration
+	logging     ServerConfiguration
+	transaction ServerConfiguration
+	web         ServerConfiguration
+}
+
+var serverConfig = ServerConfigurations{}
 
 func init() {
-	// Parse and process CLI flags (type checking is implicit)
-	flag.IntVar(&serverState.accountDbPort, "accountdbport", -1, "[REQUIRED] the port on which the USER ACCOUNT DATABASE server is running, eg. --accountdbport=8080")
-	flag.IntVar(&serverState.loggingDbPort, "loggingdbport", -1, "[REQUIRED] the port on which the LOGGING DATABASE server is running, eg. --loggingdbport=8081")
-	flag.IntVar(&serverState.httpPort, "httpport", 80, "[optional -- default is port 80] the port on which *this* HTTP server is running, eg. --httpport=80")
-	flag.IntVar(&serverState.txPort, "txport", -1, "[REQUIRED] the port on which the TRANSACTION server is running, eg. --txport=8082")
-	flag.Parse()
+	// Parse and process environment variables from config file
+	configurationFilename := ".env"
 
-	// Enforce required flags
-	if serverState.accountDbPort == -1 || serverState.txPort == -1 || serverState.loggingDbPort == -1 {
-		flag.PrintDefaults()
-		os.Exit(1)
+	err := godotenv.Load(configurationFilename)
+	if err != nil {
+		log.Fatalf("Error loading %q config file", configurationFilename)
 	}
+
+	serverConfig.database.ipAddress = os.Getenv("DATABASE_IP_ADDRESS")
+	serverConfig.database.port = os.Getenv("DATABASE_PORT")
+	serverConfig.logging.ipAddress = os.Getenv("LOGGING_IP_ADDRESS")
+	serverConfig.logging.port = os.Getenv("LOGGING_PORT")
+	serverConfig.transaction.ipAddress = os.Getenv("TRANSACTION_IP_ADDRESS")
+	serverConfig.transaction.port = os.Getenv("TRANSACTION_PORT")
+	serverConfig.web.ipAddress = os.Getenv("WEB_IP_ADDRESS")
+	serverConfig.web.port = os.Getenv("WEB_PORT")
 }
 
 func main() {
-	log.Printf("LOG: Server starting with config: %+v\n", serverState)
+	log.Printf("Server starting with config: \n%+v\n\n", serverConfig)
 
 	// Fire up server
-	log.Printf("LOG: HTTP server listening on http://localhost:%d/\n", serverState.httpPort)
+	log.Printf("HTTP server listening on http://%s:%s/\n\n", serverConfig.web.ipAddress, serverConfig.web.port)
 	http.HandleFunc("/", requestRouter)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(serverState.httpPort), nil))
+	go log.Fatal(http.ListenAndServe(":"+serverConfig.web.port, nil))
 }
 
 // requestRouter routes the request to the appropriate handler based on its HTTP method
 func requestRouter(w http.ResponseWriter, r *http.Request) {
-	log.Printf("LOG: Received %s request.\n", r.Method)
+	log.Printf("Received %s request\n", r.Method)
+
 	switch r.Method {
-	case "POST":
-		commandProcessor(w, r)
-	case "GET":
+	case http.MethodPost:
+		// POST requests come from UI and/or workload generator:
+		log.Println("Routing POST request to commandHandler")
+		commandHandler(w, r)
+	// GET requests are only expected from UI:
+	case http.MethodGet:
+		log.Println("Routing GET request to userInterfaceHandler")
 		userInterfaceHandler(w, r)
 	default:
+		// No other HTTP methods are supported:
+		log.Printf("HTTP method %q not supported\n", r.Method)
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
-// commandProcessor processes a JSON command and forwards it to the transaction server
-func commandProcessor(w http.ResponseWriter, r *http.Request) {
+// JSONPayload represents the expected JSON body of a request
+type JSONPayload struct {
+	Command     string `json: "command"`
+	UserID      string `json: "userID,omitempty"`
+	Amount      string `json: "amount,omitempty"`
+	StockSymbol string `json: "stockSymbol,omitempty"`
+	Filename    string `json: "filename,omitempty"`
+}
+
+// requestDecoder decodes a JSON command
+func commandHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Handling JSON body of %s request", r.Method)
+
+	var requestBodyJSON = JSONPayload{}
+
 	// Read request body
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
+		log.Panic(err)
 	}
 	defer r.Body.Close()
 
-	// Parse and validate request
-	commandID, parameters := commonlib.GetCommandFromMessage(requestBody)
-
-	if validateCommandAndParameters(commandID, parameters) {
-		// TODO: Check return value and handle retries
-		forwardCommandToTransactionServer(commandID, parameters)
-		w.WriteHeader(http.StatusOK)
-	} else {
-		// TODO: Handle failed validations manually
+	// Unmarshal JSON directly into JSONPayload struct
+	err = json.Unmarshal(requestBody, &requestBodyJSON)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		log.Panic(err)
 	}
+
+	log.Printf("Message received was: %+v", requestBodyJSON)
+	// TODO: Validate parameters
+	buildAndSendMessage(requestBodyJSON)
 }
 
 // userInterfaceHandler serves the user interface HTML file
 func userInterfaceHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving user interface")
 	http.ServeFile(w, r, "www/index.html")
-}
-
-// forwardCommandToTransactionServer forwards the received message to the Transaction Server
-func forwardCommandToTransactionServer(commandID uint8, parameters commonlib.CommandParameter) (bool, error) {
-	sendableCommand := commonlib.GetSendableCommand(commandID, parameters)
-	response, err := commonlib.SendCommand("GET", "application/json", serverState.txPort, sendableCommand)
-
-	log.Println("LOG: Sent command: ", sendableCommand)
-	log.Println("LOG: Received response: ", response)
-
-	if err != nil {
-		log.Println("LOG: Forwarding message to Transaction Server failed with error: ", err)
-		return false, err
-	}
-	log.Println("LOG: Forwarding message to Transaction Server succeeded.")
-	return true, err
-}
-
-// saveToFile exports a byte array to a file on local disk
-func saveToFile(content []byte, filename string) {
-	file, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	bytesWritten, err := file.Write(content)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("LOG: Wrote %d bytes to logfile: %q\n", bytesWritten, filename)
-}
-
-// validateCommand ensures that the command is one of the known/valid commands and has its necessary parameters
-func validateCommandAndParameters(commandID uint8, parameters commonlib.CommandParameter) bool {
-	// TODO: Ensure all parameters pertaining to the specific command are present and valid
-
-	return true
-}
-
-// validateAmount ensures that the amount specified in command is valid
-func validateAmount(amount string) bool {
-	// TODO: Must be non-negative
-	// TODO: Must not contain non-numerical characters (including "$")
-	// TODO: Must contain two decimal places <---(Do we want to round, or reject, if <> 2 decimal places?)
-
-	return true
-}
-
-// validateUserID ensures that the user specified in command is valid
-func validateUserID(userID string) bool {
-	// TODO: Create user if not exists <--(or does the client issue a "CREATE" command?)
-
-	return true
-}
-
-// validateStockSymbol ensures that the stock symbol specified in command is valid
-func validateStockSymbol(stockSymbol string) bool {
-	// TODO: Must be 1 - 3 alphanumeric, case insensitive
-
-	return true
-}
-
-// getDumplogForUser retrieves the transaction history for a specific user from the database and saves it to a logfile
-func getDumplogForUser(userid string) {
-
-	// TODO: Open connection to DB
-	// TODO: Query DB eg. db.Query("SELECT * FROM transactions WHERE userid = $1", userid)
-	// TODO: Write returned rows to <filename>
-	// TODO: Close connection to DB
-}
-
-// getDumplogForAll retrieves the transaction history for all users from the database and saves it to a logfile
-func getDumplogForAll() {
-	// TODO: Open connection to DB
-	// TODO: Query DB eg. db.Query("SELECT * FROM transactions)
-	// TODO: Write returned rows to <filename>
-	// TODO: Close connection to DB
 }

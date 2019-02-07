4664d4b2c8840227e22f383fade31e8f588fb642
stvhwrd <stvhwrd@outlook.com>
2019-02-07 15:53:39 -0800

    Reduce spaghetti, clean up routing

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+
+	commonlib "github.com/kurtd5105/SENG-468-Common-Lib"
 )
 
 // ServerNetwork holds information about the system's servers' network addresses
@@ -14,27 +17,24 @@ type ServerNetwork struct {
 	databaseServerAddressAndPort    string
 	loggingServerAddressAndPort     string
 	transactionServerAddressAndPort string
-	webServerAddress                string
-	webServerPort                   string
+	webServerPort                   int
 }
 
-var serverNetworkConfig = ServerNetwork{}
+var state = ServerNetwork{}
 
 func init() {
 	// Parse and process CLI flags
-	flag.StringVar(&serverNetworkConfig.databaseServerAddressAndPort, "db", "", "[REQUIRED] the IP address and port on which the USER ACCOUNT DATABASE server is running, eg. -db=localhost:8080")
-	flag.StringVar(&serverNetworkConfig.loggingServerAddressAndPort, "log", "", "[REQUIRED] the IP address and port on which the LOGGING DATABASE server is running, eg. -log=localhost:8081")
-	flag.StringVar(&serverNetworkConfig.webServerAddress, "webip", "", "[REQUIRED] the IP address on which *this* HTTP server is running, eg. -webip=localhost")
-	flag.StringVar(&serverNetworkConfig.webServerPort, "webport", "", "[REQUIRED] the IP address and port on which *this* HTTP server is running, eg. -webport=localhost:80")
-	flag.StringVar(&serverNetworkConfig.transactionServerAddressAndPort, "tx", "", "[REQUIRED] the IP address and port on which the TRANSACTION server is running, eg. -tx=localhost:8082")
+	flag.StringVar(&state.databaseServerAddressAndPort, "db", "", "[REQUIRED] the IP address and port on which the USER ACCOUNT DATABASE server is running, eg. -db=localhost:8080")
+	flag.StringVar(&state.loggingServerAddressAndPort, "log", "", "[REQUIRED] the IP address and port on which the LOGGING DATABASE server is running, eg. -log=localhost:8081")
+	flag.IntVar(&state.webServerPort, "port", -1, "[REQUIRED] the port on which *this* HTTP server is running, eg. -port=localhost:80")
+	flag.StringVar(&state.transactionServerAddressAndPort, "tx", "", "[REQUIRED] the IP address and port on which the TRANSACTION server is running, eg. -tx=localhost:8082")
 	flag.Parse()
 
 	// Enforce required flags
-	if serverNetworkConfig.databaseServerAddressAndPort == "" ||
-		serverNetworkConfig.transactionServerAddressAndPort == "" ||
-		serverNetworkConfig.loggingServerAddressAndPort == "" ||
-		serverNetworkConfig.webServerAddress == "" ||
-		serverNetworkConfig.webServerPort == "" {
+	if state.databaseServerAddressAndPort == "" ||
+		state.transactionServerAddressAndPort == "" ||
+		state.loggingServerAddressAndPort == "" ||
+		state.webServerPort < 0 {
 		log.Println("Error: Required flags were not provided at runtime")
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -42,13 +42,16 @@ func init() {
 }
 
 func main() {
-	log.Printf("Server starting with config: \n%+v\n\n", serverNetworkConfig)
+	log.Printf("Server starting with config: \n%+v\n\n", state)
+
+	portString := strconv.Itoa(state.webServerPort)
 
 	// Fire up server
-	log.Printf("HTTP server listening on http://%s/\n\n", serverNetworkConfig.webServerAddress)
-	// commonlib.StartServer(serverNetworkConfig.webServerPort, requestRouter)
+	log.Printf("HTTP server listening on http://localhost:%d/\n\n", state.webServerPort)
+
+	// TODO: figure out why commonlib.StartServer doesn't work for this
 	http.HandleFunc("/", requestRouter)
-	go log.Fatal(http.ListenAndServe(":"+serverNetworkConfig.webServerPort, nil))
+	go log.Fatal(http.ListenAndServe(":"+portString, nil))
 }
 
 // requestRouter routes the request to the appropriate handler based on its HTTP method
@@ -80,7 +83,7 @@ type JSONPayload struct {
 	Filename    string `json: "filename,omitempty"`
 }
 
-// requestDecoder decodes a JSON command
+// requestDecoder decodes a JSON command and forwards it appropriately
 func commandHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Handling JSON body of %s request", r.Method)
 
@@ -90,6 +93,7 @@ func commandHandler(w http.ResponseWriter, r *http.Request) {
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error reading request body"))
 		log.Panic(err)
 	}
 	defer r.Body.Close()
@@ -98,12 +102,33 @@ func commandHandler(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(requestBody, &requestBodyJSON)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error unmarshaling request body"))
 		log.Panic(err)
 	}
 
 	log.Printf("Message received was: %+v", requestBodyJSON)
-	// TODO: Validate parameters
-	buildAndSendMessage(requestBodyJSON)
+
+	// TODO: Use commonlib full implementation of building message
+	commandID := getCommandID(requestBodyJSON.Command, requestBodyJSON.UserID)
+	parameters := commonlib.CommandParameter{}
+
+	parameters.UserID = requestBodyJSON.UserID
+	parameters.Amount = requestBodyJSON.Amount
+	parameters.Filename = requestBodyJSON.Filename
+	parameters.StockSymbol = requestBodyJSON.StockSymbol
+
+	destinationServer := getDestinationServer(commandID)
+
+	log.Printf("Sending POST request with command to %s/ \n\n", destinationServer)
+	log.Printf("\tCOMMAND: %s (#%d)\n", requestBodyJSON.Command, commandID)
+	log.Printf("\tPARAMETERS: %+v\n", parameters)
+
+	response, err := commonlib.SendCommand("POST", "application/json", destinationServer, commonlib.GetSendableCommand(commandID, parameters))
+	if err != nil {
+		log.Printf("-- Error sending command --")
+		panic(err)
+	}
+	log.Printf("Received response:\n%s\n", response)
 }
 
 // userInterfaceHandler serves the user interface HTML file
